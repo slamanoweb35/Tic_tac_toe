b8d1657044fe5bc5332dc5f116128d4b2bec9903
bruno <ballogggamer@gmail.com>
2023-04-16 18:21:24 -0300

    ADD: implement compoenent CallPopUp

---
+++ src/components/callPopUp.tsx
@@ -0,0 +1,15 @@
+import data_popUp from "../types/dataPopUp";
+import PopUp from "./popUp";
+
+const CallPopUp = () => {
+  return (<>
+    <PopUp
+      hidden={data_popUp.hidden}
+      message={data_popUp.message}
+      nick={data_popUp.nick}
+      type={data_popUp.type}
+    />
+  </>)
+}
+
+export default CallPopUp;
