fe55ea19980e519702ae3bd6e172a5f96cea252a
bruno <ballogggamer@gmail.com>
2023-04-27 21:09:18 -0300

    ADD: implement update comoponent

--- src/components/placeBorder.tsx
+++ src/components/placeBorder.tsx
@@ -1,29 +1,37 @@
 import play from "../types/play"
 import currentRoom from "../types/room"
 import perfil from "../types/account"
-import { FC } from "react"
+import { useReducer, useEffect, useState } from "react"
+import updatePlaceBorder from "../types/updatePlaceBorder"
+import place_border from "../types/placeBorder"
 import "../css/placeBorder.css"
 
-interface PlaceBorderProp {
-  /** your points*/
-  you:number,
-  /** empty on the match*/
-  empty:number,
-  /** opponent points*/
-  opponent:number,
-}
+const Place_border = () => {
+  var [points, setPoints] = useState(place_border);
+  var [, forceUpdate] = useReducer(some => some + 1, 0);
 
-const Place_border:FC<PlaceBorderProp> = (prop) => {
+  useEffect(() => {
+    let time = setInterval(() => {
+      if (!updatePlaceBorder.update) return;
+
+      updatePlaceBorder.update = false;
+      setPoints(place_border);
+      forceUpdate();
+    },500);
+
+    return () => clearInterval(time);
+  })
+
   return (<>
     {play.playing == true ? (<div id="place_border">
       <div id="you">
-        {perfil.getNickname()} <span className="Span">{prop.you}</span>
+        {perfil.getNickname()} <span className="Span">{points.you}</span>
       </div>
       <div id="empty">
-        Empty <span className="Span" id="middle">{prop.empty}</span>
+        Ties <span className="Span" id="middle">{points.ties}</span>
       </div>
       <div id="opponent">
-        {currentRoom.opponent.nick} <span className="Span">{prop.opponent}</span>
+        {currentRoom.opponent.nick} <span className="Span">{points.opponnet}</span>
       </div>
     </div>)
       : <h1 id="warn">You are not inside a room</h1>}
