61ac7be89295bc1ccb671b460d2a8e586839e8ae
bruno <ballogggamer@gmail.com>
2023-05-01 18:54:57 -0300

    ADD: implement direction name

--- src/components/redirect.tsx
+++ src/components/redirect.tsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export var Redirect = {
-    value: false
+    value: false,
+    to:""
 }
 
 const RedirectClient = () => {
@@ -11,8 +12,10 @@ const RedirectClient = () => {
     useEffect(() => {
         let time = setInterval(() => {
             if (!Redirect.value) return;
+            
+            navigate("/"+Redirect.to);
             Redirect.value = false;
-            navigate("/playing");
+            Redirect.to = "";
         },200) 
 
         return () => clearInterval(time);
