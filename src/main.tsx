da5d962b4e41f1827cb5b5afbefc41b2d2699f55
bruno <ballogggamer@gmail.com>
2023-06-13 19:48:57 -0300

    ADD: implement root

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import State from './state';
+import './css/index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <State/>
+  </React.StrictMode>
+)
